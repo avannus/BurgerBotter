6adac14be56dba051857dc6e3a0a3f5f517934a3
Andrew <avannus@gmail.com>
2022-04-14 17:46:34 -0400

    interactions removed from index.js

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,6 +1,17 @@
 module.exports = {
   name: 'interactionCreate',
-  execute(interaction) {
+  execute(interaction, client) {
     console.log(`${interaction.user.tag} in #${interaction.channel.name} triggered an interaction.`);
+    const command = client.commands.get(interaction.commandName);
+    if (!command) return;
+    try {
+      command.execute(interaction);
+    } catch (error) {
+      console.error(error);
+      interaction.reply({
+        content: 'There was an error while executing this command!',
+        ephemeral: true,
+      });
+    }
   },
 };
