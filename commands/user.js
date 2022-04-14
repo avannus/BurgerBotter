8ad28e8e419c161898fde2d02df8e2339a653903
Andrew <avannus@gmail.com>
2022-04-14 16:02:06 -0400

    refactor

---
+++ commands/user.js
@@ -0,0 +1,12 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('user')
+    .setDescription('Replies with user info'),
+  async execute(interaction) {
+    await interaction.reply(
+      `Your username: ${interaction.user.tag}\n`
+      + `Your ID: ${interaction.user.id}`);
+  },
+};
